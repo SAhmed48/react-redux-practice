2df359487380e1de1ee258af7960f5d8e6f76641
Salman Ahmed <salmanahmed4858@gmail.com>
2020-08-14 23:48:04 +0500

    Remove console warnings.

--- advance-redux/src/store/index.js
+++ advance-redux/src/store/index.js
@@ -1,13 +1,10 @@
 import configureAppStore from './config';
 import * as apiActions from './api';
-import {bugAdded, bugResolved, bugRemoved, 
-        getUnresolvedBugs, getBugById } from './bugs';
 
 const store = configureAppStore();
 
-const unsubscribe = store.subscribe(() => {
-    // Subscription of store change to update UI
-});
+// Subscription of store change to update UI.
+const unsubscribe = store.subscribe(() => {});
 
 // api call using api middleware
 store.dispatch(apiActions.apiCallStart({
@@ -17,45 +14,5 @@ store.dispatch(apiActions.apiCallStart({
     onSuccess: 'bugAdded'
 }));
 
-// add bug action
-// store.dispatch(bugAdded({
-//     id: 1, 
-//     description: 'Bug 1', 
-//     resolved: false 
-// }));
-
-// store.dispatch(bugAdded({
-//     id: 2, 
-//     description: 'Bug 2', 
-//     resolved: false 
-// }));
-
-// store.dispatch(bugAdded({
-//     id: 3, 
-//     description: 'Bug 3', 
-//     resolved: false 
-// }));
-
-// console.log('Bug Added',store.getState());
-
-// // BUG resolved 
-// store.dispatch(bugResolved({
-//     id: 1
-// }));
-
-// console.log('Bug Resolved',store.getState());
-
-
-// // remove bug action
-// store.dispatch(bugRemoved({
-//     id: 1
-// }));
-
-// console.log('Bug removed',store.getState());
-
 // avoid memory leak
 unsubscribe();
-
-
-// console.log('Un resolved bug query', getUnresolvedBugs(store.getState()));
-// console.log('Get BUG by ID', getBugById(store.getState(), 2));
